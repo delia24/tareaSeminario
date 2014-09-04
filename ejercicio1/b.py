608fa5dd99cfd0148f412ba8805dd983c19616f9
mmiicckkyy <micky_porcel_7287@hotmail.com>
2014-09-03 22:41:07 -0400

    tarea seminario

---
+++ ejercicio1/b.py
@@ -0,0 +1,8 @@
+Python 2.7.8 (default, Jun 30 2014, 16:03:49) [MSC v.1500 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> for (int i=20; i>=2; i-=2){
+	
+SyntaxError: invalid syntax
+>>>  for (int i=2; i <=20; i += 2) {  
+        System.out.println(i);
+    } 
---
+++ ejercicio2/ejer.py
@@ -0,0 +1,9 @@
+Python 2.7.8 (default, Jun 30 2014, 16:03:49) [MSC v.1500 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> list (range(1, 20))
+[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
+>>> list (range(2, 0, 20))
+[]
+>>> list(range(20, 0, -2))
+[20, 18, 16, 14, 12, 10, 8, 6, 4, 2]
+>>> 
